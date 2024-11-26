6cdc829ded25fe42975a92b9d9d940d3c8c74883
Angelo Ndifor Acheregwa Ndonui <angelondifor.acheregwandonui@etu.cegepdrummond.ca>
2024-11-25 19:07:27 -0500

    Crées l'utilisateur 'Romaric'

--- footrise_create.sql
+++ footrise_create.sql
@@ -1,3 +1,6 @@
-create schema football;
-set search_path to football;
+create role Romaric with login password 'andre';
+
+grant connect on database postgres to Romaric;
+grant usage on schema footrise to Romaric;
+grant select, insert, update on all tables in schema public to Romaric;
 
